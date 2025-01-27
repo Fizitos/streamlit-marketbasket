3bac0e3487d8b888d6239a94f0546174d4e72ae6
Fizitos <133029080+Fizitos@users.noreply.github.com>
2025-01-27 19:31:03 +0700

    Update steamlitapmarket.py

--- steamlitapmarket.py
+++ steamlitapmarket.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from streamlit_option_menu import option_menu
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 
