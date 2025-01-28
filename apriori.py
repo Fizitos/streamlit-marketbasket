80ece474c0328759ba0e27751240caf2d9baf86f
Fizitos <133029080+Fizitos@users.noreply.github.com>
2025-01-28 15:50:32 +0700

    Update apriori.py

--- apriori.py
+++ apriori.py
@@ -17,7 +17,7 @@ st.markdown("""
 </style>
 
 <style>[data-testid="stAppViewContainer"] {
-        background-image: url("https://plus.unsplash.com/premium_vector-1736908116123-1ab63654ee99?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+        background-image: url("https://unsplash.com/illustrations/art-design-abstract-purple-stains-and-lines-background-vector-illustration-rn2I7sxepQE"),
             linear-gradient(#4d4d4d, transparent),
             linear-gradient(to top left, #333333, transparent),
             linear-gradient(to top right, #4d4d4d, transparent);
